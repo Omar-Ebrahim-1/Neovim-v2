38b736a613c5a7b32390f6b9ab9fb62dfd562fef
__OmarEbrahim__ <omarkhaled200321@gmail.com>
2023-11-12 11:15:13 -0500

    Finished nvim-cmp?

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -47,18 +47,24 @@ cmp.setup({
     { name = 'buffer', keyword_length = 5 },
   }),
   formatting = {
-    format = lspkind.cmp_format({
-      mode = 'symbol', -- show only symbol annotations
-      maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-      ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-
-      -- The function below will be called before any actual modifications from lspkind
-      -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-      before = function (entry, vim_item)
-        return vim_item
-      end
-    })
-  }
+    format = lspkind.cmp_format {
+      with_text = true,
+      menu = {
+        buffer = "[buf]",
+        nvim_lsp = "[LSP]",
+        nvim_lua = "[api]",
+        path = "[path]",
+        luasnip = "[snip]",
+        gh_issues = "[issues]",
+        tn = "[TabNine]",
+        eruby = "[erb]",
+        cody = "[cody]",
+      },
+    }
+  },
+  experimental = {
+    native_menu = false,
+  },
 })
 
 -- Set configuration for specific filetype.
