376c84b6636da7e935bc9a45c5f685b2dc9d73c7
__OmarEbrahim__ <omarkhaled200321@gmail.com>
2023-11-12 17:42:24 -0500

    commented unused variables

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -139,27 +139,26 @@ cmp.setup.filetype('gitcommit', {
 -- LSP Snippets
 --------------------------------------------------------------------------------
 local ls = require 'luasnip'
-local types = require 'luasnip.util.types'
 -- some shorthands...
 local s = ls.snippet
-local sn = ls.snippet_node
+-- local sn = ls.snippet_node
 local t = ls.text_node
 local i = ls.insert_node
 local f = ls.function_node
 local c = ls.choice_node
-local d = ls.dynamic_node
-local r = ls.restore_node
-local l = require("luasnip.extras").lambda
+-- local d = ls.dynamic_node
+-- local r = ls.restore_node
+-- local l = require("luasnip.extras").lambda
 local rep = require("luasnip.extras").rep
-local p = require("luasnip.extras").partial
-local m = require("luasnip.extras").match
-local n = require("luasnip.extras").nonempty
-local dl = require("luasnip.extras").dynamic_lambda
+-- local p = require("luasnip.extras").partial
+-- local m = require("luasnip.extras").match
+-- local n = require("luasnip.extras").nonempty
+-- local dl = require("luasnip.extras").dynamic_lambda
 local fmt = require("luasnip.extras.fmt").fmt
-local fmta = require("luasnip.extras.fmt").fmta
+-- local fmta = require("luasnip.extras.fmt").fmta
 local types = require("luasnip.util.types")
-local conds = require("luasnip.extras.conditions")
-local conds_expand = require("luasnip.extras.conditions.expand")
+-- local conds = require("luasnip.extras.conditions")
+-- local conds_expand = require("luasnip.extras.conditions.expand")
 
 ls.setup({
 	keep_roots = true,
@@ -225,6 +224,7 @@ ls.config.set_config {
 vim.keymap.set({"i", "s"}, "<C-L>", function() ls.jump( 1) end, {silent = true})
 vim.keymap.set({"i", "s"}, "<C-J>", function() ls.jump(-1) end, {silent = true})
 vim.keymap.set({"i", "s"}, "<C-K>", function()
+
 	if ls.choice_active() then
 		ls.change_choice(1)
 	end
