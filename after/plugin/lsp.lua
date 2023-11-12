cd8fea266dc24a39f35eee46aca7d306afc666ee
__OmarEbrahim__ <omarkhaled200321@gmail.com>
2023-11-12 14:29:14 -0500

    Snippets work!!!

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -9,6 +9,8 @@ local on_attach = function()
     vim.keymap.set('n', '<leader>dk', vim.diagnostic.goto_prev, {buffer = 0})
     vim.keymap.set('n', 'grr', vim.lsp.buf.rename, {buffer = 0})
 end
+local ls = require 'luasnip'
+local types = require 'luasnip.util.types'
 
 
 --------------------------------------------------------------------------------
@@ -76,4 +78,101 @@ cmp.setup.filetype('gitcommit', {
   }, {
     { name = 'buffer' },
   })
+})
+
+--------------------------------------------------------------------------------
+-- LSP Snippets
+--------------------------------------------------------------------------------
+local ls = require("luasnip")
+-- some shorthands...
+local s = ls.snippet
+local sn = ls.snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local c = ls.choice_node
+local d = ls.dynamic_node
+local r = ls.restore_node
+local l = require("luasnip.extras").lambda
+local rep = require("luasnip.extras").rep
+local p = require("luasnip.extras").partial
+local m = require("luasnip.extras").match
+local n = require("luasnip.extras").nonempty
+local dl = require("luasnip.extras").dynamic_lambda
+local fmt = require("luasnip.extras.fmt").fmt
+local fmta = require("luasnip.extras.fmt").fmta
+local types = require("luasnip.util.types")
+local conds = require("luasnip.extras.conditions")
+local conds_expand = require("luasnip.extras.conditions.expand")
+
+ls.setup({
+	keep_roots = true,
+	link_roots = true,
+	link_children = true,
+
+	-- Update more often, :h events for more info.
+	update_events = "TextChanged,TextChangedI",
+	-- Snippets aren't automatically removed if their text is deleted.
+	-- `delete_check_events` determines on which events (:h events) a check for
+	-- deleted snippets is performed.
+	-- This can be especially useful when `history` is enabled.
+	delete_check_events = "TextChanged",
+	ext_opts = {
+		[types.choiceNode] = {
+			active = {
+				virt_text = { { "choiceNode", "Comment" } },
+			},
+		},
+	},
+	-- treesitter-hl has 100, use something higher (default is 200).
+	ext_base_prio = 300,
+	-- minimal increase in priority.
+	ext_prio_increase = 1,
+	enable_autosnippets = true,
+	-- mapping for cutting selected text so it's usable as SELECT_DEDENT,
+	-- SELECT_RAW or TM_SELECTED_TEXT (mapped via xmap).
+	store_selection_keys = "<Tab>",
+	-- luasnip uses this function to get the currently active filetype. This
+	-- is the (rather uninteresting) default, but it's possible to use
+	-- eg. treesitter for getting the current filetype by setting ft_func to
+	-- require("luasnip.extras.filetype_functions").from_cursor (requires
+	-- `nvim-treesitter/nvim-treesitter`). This allows correctly resolving
+	-- the current filetype in eg. a markdown-code block or `vim.cmd()`.
+	ft_func = function()
+		return vim.split(vim.bo.filetype, ".", true)
+	end,
+})
+
+ls.config.set_config {
+  -- This tells LuaSnip to remember to keep around the last snippet.
+  -- You can jump back into it even if you move outside of the selection
+  history = true,
+
+  -- This one is cool cause if you have dynamic snippets, it updates as you type!
+  updateevents = "TextChanged,TextChangedI",
+
+  -- Autosnippets:
+  enable_autosnippets = true,
+
+  -- Crazy highlights!!
+  -- #vid3
+  -- ext_opts = nil,
+  ext_opts = {
+    [types.choiceNode] = {
+      active = {
+        virt_text = { { " « ", "NonTest" } },
+      },
+    },
+  },
+}
+vim.keymap.set({"i"}, "<M-L>", function() ls.expand() end, {silent = true})
+vim.keymap.set({"i", "s"}, "<M-K>", function() ls.jump(1) end, {silent = true})
+vim.keymap.set({"i", "s"}, "<M-J>", function() ls.jump(-1) end, {silent = true})
+
+ls.add_snippets("all",
+{
+  -- basic, don't need to know anything else
+  --    arg 1: string
+  --    arg 2: a node
+  s("simple", t "wow, you were right!"),
 })
